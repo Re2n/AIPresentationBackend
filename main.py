62ea392b3b8377b5ba6b901d308c0131114fce32
Re2n <retonur@mail.ru>
2024-10-14 20:05:23 +0300

    update

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ app.include_router(user_router)
 admin.add_view(UserAdmin)
 admin.add_view(ActionAdmin)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
